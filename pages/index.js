ce6921b8647db5e074d762c11085952e74210289
Dev Gile <gillead.dev@hotmail.com>
2024-12-02 01:35:38 +0000

    atualizacao

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Meu amor, eu te amo mais do que tudo, es a minha paixao o amor da minha vida e eu quero me casar com voce, eu te amo muito</h1>
+    return <h1>te amo</h1>
 }
 
 export default Home;
